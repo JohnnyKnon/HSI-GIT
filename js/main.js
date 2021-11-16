9dce7b0e2dfc8703e3720a67e73c638421ed8e9d
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2021-11-16 11:27:40 +0900

    Modify Card

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{ // 클릭한 이벤트 추가
+    const target = event.target; //target (눌렀을때에 해당 타겟에 이벤트)
+    const link = target.dataset.link; //눌려진 타켓에 링크 값
+    if(link == null){
+        return;
+    }
+   scrollIntoView(link);
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector); //scrollIntoView엘리멘트 의 부모 컨테이너로 스크롤되는 메소드
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
