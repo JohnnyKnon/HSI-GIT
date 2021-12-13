2ef6fa551c201fb346550304e4518cab485d1398
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2021-12-13 12:58:30 +0900

    need more
    1. Slider Remaking
    2. Group Img change

--- js/slider_thyss.js
+++ js/slider_thyss.js
@@ -4,17 +4,17 @@ showSlidesThyss();
 
 function showSlidesThyss() {
   var i;
-  var slides = document.getElementsByClassName("mySlides-thyss");
-  var dots = document.getElementsByClassName("dot-thyss");
+  const slides = document.getElementsByClassName("mySlides-thyss");
+  const dots = document.getElementsByClassName("dot-thyss");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("active-thyss", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+  setTimeout(showSlides, 2500); // Change image every 3 seconds
 }
