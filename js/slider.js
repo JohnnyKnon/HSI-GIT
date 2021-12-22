e5fd9f47f36bc2434331599c8f2fe7208e461f73
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2021-12-22 23:28:04 +0900

    Work Slider Modify
    Using Sliker

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.sonats__slideshow__container').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      cssEase: 'linear'
+    });
+  });
+
+  $(document).ready(function(){
+    $('.thyss__slideshow__container').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      cssEase: 'linear',
+    });
+  });
