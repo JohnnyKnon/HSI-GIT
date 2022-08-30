f1cf9c77dc0e1189825b2a76669d4182916da423
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-08-30 14:02:03 +0900

    Update mail.php

--- admin/db/mail.php
+++ admin/db/mail.php
@@ -8,26 +8,6 @@ $mailTitle			=		$_POST['contactType'];
 $mailMessage		=		"홈페이지를 통한 문의가 접수되었습니다.";
 $returnMail			=		$mailFrom;
 
-/*mb_language("ko");
-mb_internal_encoding("UTF-8");
-
-$header		= 'From:'.$mailFrom."\n";
-$header		.= "MIME-Version: 1.0\n";
-$header		.="Content-Type: multipart/mixed; boundary=\"__PHPRECIPE__\"";
-
-$body		= "--__PHPRECIPE__\r\n";
-
-$body		.="Content-Type: text/plain; charset=\"UTF-8\"\r\n";
-$body		.="\r\n";
-$body		.= $mailMessage ."\r\n";
-$body		.="--__PHPRECIPE__\r\n";
-
-if (ini_get('safe_mode')) {
-	$result = @mail($mailTo,  $body, $header);
-}else{
-    $result = @mail($mailTo,  $body, $header, '-f' .returnMail);
-}*/
-
 if(empty($mailName) || empty($mailName) || empty($mailTitle) || empty($mailMessage)){
 
   die("파라미터 에러!");
